5d326e5c358fdb86303178c46b9e2bab62b3432e
Chuma Humphrey <chumphrey17c@gmail.com>
2026-03-26 13:30:30 -0500

    initial creation of backend

---
+++ Personal_Capstone/qa-gen-backend/src/main/java/mil/army/moda/qagenbackend/QaGenBackendApplication.java
@@ -0,0 +1,13 @@
+package mil.army.moda.qagenbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaGenBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QaGenBackendApplication.class, args);
+    }
+
+}
