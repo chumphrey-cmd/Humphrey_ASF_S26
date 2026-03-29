1de68945d034ff16009ca0121eb98c3aa3ee39ec
Chuma Humphrey <chumphrey17c@gmail.com>
2026-03-29 17:55:18 -0500

    complete of Repository Layer, starting basic TDD

---
+++ Personal_Capstone/qa-gen-backend/src/test/java/mil/army/moda/qagenbackend/quiz/QuizRepositoryTest.java
@@ -0,0 +1,14 @@
+package mil.army.moda.qagenbackend.quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+public class QuizRepositoryTest {
+}
