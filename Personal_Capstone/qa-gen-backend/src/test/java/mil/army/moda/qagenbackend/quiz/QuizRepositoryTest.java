f1f02a26c8c33682041c80c38a273eafb0136c00
Chuma Humphrey <chumphrey17c@gmail.com>
2026-03-31 10:33:13 -0500

    resolved QuestionRepositoryTest, depends on a User begin created

--- Personal_Capstone/qa-gen-backend/src/test/java/mil/army/moda/qagenbackend/quiz/QuizRepositoryTest.java
+++ Personal_Capstone/qa-gen-backend/src/test/java/mil/army/moda/qagenbackend/quiz/QuizRepositoryTest.java
@@ -1,8 +1,10 @@
 package mil.army.moda.qagenbackend.quiz;
 
+import mil.army.moda.qagenbackend.question.QuestionRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jpa.test.autoconfigure.TestEntityManager;
 
 import java.util.Optional;
 
@@ -11,7 +13,5 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 public class QuizRepositoryTest {
-
-
 
 }
